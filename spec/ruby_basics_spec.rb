2db2bbe9de80d8a95ef963f05ca5e72eabd003e9
ashontell <ashontell@Businesss-MacBook-Air-15.local>
2015-04-29 19:44:07 -0400

    .

--- spec/ruby_basics_spec.rb
+++ spec/ruby_basics_spec.rb
@@ -19,17 +19,17 @@ describe "ruby" do
 
   # 2.
   it 'has a concept of truth' do 
-    expect(__)
+    expect('')
   end
 
   # 3. 
   it 'knows math' do
-    expect(__).to eq(6*7)
+    expect(42).to eq(6*7)
   end
 
   # 4. 
   it 'can remember values with variables' do
-    the_number_one = __
+    the_number_one = 1
 
     expect(the_number_one).to eq(1) 
   end
@@ -41,14 +41,16 @@ describe "ruby" do
 
     original = 2
 
-    expect(reference).to eq(__)
+    expect(reference).to eq(1)
   end
 
   # 6. 
   it 'has methods' do
 
     class Object
-      # define a method named "defined_method"
+      # define a method named 
+      def defined_method
+      end
       # within this class.
     end
 
@@ -58,7 +60,7 @@ describe "ruby" do
   # 7. 
   it 'has methods that can accept arguments' do
     def work(adjective)
-      expect(adjective).to eq(__)
+      expect(adjective).to eq("hard")
     end
 
     work("hard")
@@ -69,6 +71,8 @@ describe "ruby" do
     class Object
       # define a method named "a_method_with" that takes two arguments
       # within this class.
+      def a_method_with(hi, hello)
+      end
     end
 
     expect(Object).to respond_to(:a_method_with).with(2).arguments
@@ -77,7 +81,9 @@ describe "ruby" do
   # 9. 
   it 'has methods that return values' do
     # define a method named "always_be" that returns "Nice"
-
+    def always_be
+      "Nice"
+    end
     expect(always_be).to eq("Nice")
   end
 
@@ -88,7 +94,7 @@ describe "ruby" do
       "expert"
     end
 
-    expect(learning).to eq(__)
+    expect(learning).to eq("expert")
   end
 
   # 11.
@@ -97,7 +103,7 @@ describe "ruby" do
       state
     end
 
-    expect(keep).to eq(__)
+    expect(keep).to eq("calm")
   end
 
 end
