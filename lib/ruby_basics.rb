2c94517f8df45c99385f17c6a2bf3d3e0fa49e64
alicht <avilichtschein@gmail.com>
2015-11-16 20:47:35 -0500

    completed lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg)
+  arg = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(hi, name)
+  hi = "Hi there "
+  name = "Avi!"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  value = "expert"
 end
 
-def pizza_party
-
+def pizza_party(pepperoni="cheese")
+  pepperoni
 end
